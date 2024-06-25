e52d0fc5c6b710731ec3c2233a937b7638efd28a
Elvis-199-afk <earocutipaq@unjbg.edu.pe>
2024-06-25 09:17:10 -0500

    Agregando el segundo ejercicio

---
+++ 2do_Ejercicio.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main(){
+	srand(time(0));
+	int cantidadNumeros, maximo; 
+	cout<<"Ingrese la cantidad de numero que desea generar: ";
+	cin>>cantidadNumeros;
+	cout<<"Ingrese la cantidad maxima de los numeros a generar: ";
+	cin>>maximo;
+	cout<<"Los "<<cantidadNumeros<<" numeros aleatorios generados entre 0 y "<<maximo<<" son: "<<endl;
+	for(int i=1;i<=cantidadNumeros;i++){
+		int aleatorio=rand() % maximo + 0;
+		cout<<aleatorio<<endl;
+		
+	}
+	
+	return 0;
+}
